a6362a234da46897943f672a8b1acc347704c80f
naushu03 <sknausheenshaik@gmail.com>
2026-07-30 17:24:41 +0530

    Time: 7 ms (55.62%), Space: 13.5 MB (76.59%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -6,10 +6,8 @@ class Solution(object):
         """
         i=0
         for j in range(len(nums)): 
-            if nums[i]==0 and nums[j]!=0:
+            if nums[j]!=0:
                 nums[i],nums[j]=nums[j],nums[i]
-                i+=1
-            elif nums[i]!=0:
                 i+=1
             
             
