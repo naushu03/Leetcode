a0800d483151fee43fa7afe8b912f92036a6cbf0
naushu03 <sknausheenshaik@gmail.com>
2026-07-31 10:42:07 +0530

    Time: 0 ms (100%), Space: 19.2 MB (51.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,11 @@
 class Solution(object):
     def climbStairs(self, n):
-        
-        @cache
+        lst=[-1]*(n+1)
         def fib(n):
             if n<=1:
                 return 1
-            if n==2:
-                return 2
-            return fib(n-1)+fib(n-2)
+            if lst[n]!=-1:
+                return lst[n]
+            lst[n]=fib(n-1)+fib(n-2)
+            return lst[n]
         return fib(n)
