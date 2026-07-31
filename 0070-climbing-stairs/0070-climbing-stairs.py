a33afb56242adc55ad9148be75b93e1f2933d2d9
naushu03 <sknausheenshaik@gmail.com>
2026-07-31 10:48:21 +0530

    Time: 0 ms (100%), Space: 19 MB (97.86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,6 @@
 class Solution(object):
     def climbStairs(self, n):
-        lst=[-1]*(n+1)
-        def fib(n):
-            if n<=1:
-                return 1
-            if lst[n]!=-1:
-                return lst[n]
-            lst[n]=fib(n-1)+fib(n-2)
-            return lst[n]
-        return fib(n)
+        dp=[1]*(n+1)
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
