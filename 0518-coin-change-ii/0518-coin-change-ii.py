2d93fde8a0c54f1bf179057f8b19dfafec3a824c
naushu03 <sknausheenshaik@gmail.com>
2026-08-01 13:28:37 +0530

    Time: 447 ms (46.18%), Space: 65.2 MB (33.04%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -6,7 +6,8 @@ class Solution:
             if i%coins[0]==0:
                 dp[0][i]=1
         for i in range(1,len(coins)):
-            for j in range(0,amount+1):
+            dp[i][0]=1
+            for j in range(1,amount+1):
                 if coins[i]>j:
                     dp[i][j]=dp[i-1][j]
                 else:
